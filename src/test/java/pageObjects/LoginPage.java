5eb61fa4acb5883409cb8fb31de75b96f3330b97
OliviaVasile <71640044+OliviaVasile@users.noreply.github.com>
2021-03-11 15:32:12 +0200

    afterGetElementMessage

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -19,8 +19,8 @@ public class LoginPage {
     private WebDriver driver;
     WebDriverWait wait;
 
-//@FindBy ( xpath = "//*[@id=\"cookie-monster\"]/div/div[2]/a")
-//WebElement cookieMess;
+    @FindBy(xpath = "//*[@id=\"cookie-monster\"]/div/div[2]/a")
+    WebElement cookieMess;
 
     @FindBy(id = "email")
     WebElement usernameInput;
@@ -29,7 +29,7 @@ public class LoginPage {
     @FindBy(id = "send2")
     WebElement Autentificare;
 
-    @FindBy(xpath = "/html/body/div[2]/div[4]/div[1]/div[3]/div/div[2]/div[2]/ul/li/ul/li")
+    @FindBy(xpath = "//*[@id=\"top\"]/body/div[3]/div[3]/div[1]/div[3]/div/div[2]/div[2]/ul/li/ul/li/span")
     WebElement errGeneral;
     @FindBy(id = "advice-required-entry-pass")
     WebElement errPassword;
@@ -43,33 +43,55 @@ public class LoginPage {
     }
 
     public void login (String username , String password) {
+//        waitForError();
+
         usernameInput.clear();
         usernameInput.sendKeys(username);
         passwordInput.clear();
         passwordInput.sendKeys(password);
         Autentificare.submit();
-        waitForError();
+//        fluentWait(By.id("advice-required-entry-pass"));
+//        fluentWait(By.id("advice-required-entry-email"));
+//        fluentWait(By.xpath("//*[@id=\"top\"]/body/div[3]/div[3]/div[1]/div[3]/div/div[2]/div[2]/ul/li/ul/li/span"));
+
 
     }
 
-    public String waitForError() {
-        int retry = 0;
-        String error="";
-        while (retry < 3) {
-            try {
-//                error=SeleniumUtils.waitForGenericElement(driver, By.xpath("/html/body/div[2]/div[4]/div[1]/div[3]/div/div[2]/div[2]/ul/li/ul/li"), 15).getText();
-                error=SeleniumUtils.waitForGenericElement(driver, By.id("advice-required-entry-pass"), 20).getText();
-//                error=SeleniumUtils.waitForGenericElement(driver, By.id("advice-required-entry-email"), 15).getText();
-                retry = 4;
-            } catch (ElementClickInterceptedException | NoSuchElementException e) {
-                retry++;
-            }
-        }
-        return error;}
+//    public WebElement fluentWait (final By locator) {
+//        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+//                .withTimeout(10 , TimeUnit.SECONDS)
+//                .pollingEvery(5 , TimeUnit.SECONDS)
+//                .ignoring(NoSuchElementException.class);
+//
+//        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+//            public WebElement apply (WebDriver driver) {
+//                return driver.findElement(locator);
+//            }
+//        });
+//
+//        return foo;
+//    }
 
-    public void initializeGeneralErrorElement ( ) {
-        errGeneral = SeleniumUtils.waitForGenericElement(driver , By.xpath("/html/body/div[5]/div/div/div/div/form/div/div[2]/div") , 15);
-    }
+    ;
+
+//    public String waitForError() {
+//        int retry = 0;
+//        String error="";
+//        while (retry < 3) {
+//            try {
+////                error=SeleniumUtils.waitForGenericElement(driver, By.xpath("/html/body/div[2]/div[4]/div[1]/div[3]/div/div[2]/div[2]/ul/li/ul/li"), 15).getText();
+//                error=SeleniumUtils.waitForGenericElement(driver, By.id("advice-required-entry-pass"), 20).getText();
+////                error=SeleniumUtils.waitForGenericElement(driver, By.id("advice-required-entry-email"), 15).getText();
+//                retry = 4;
+//            } catch (ElementClickInterceptedException | NoSuchElementException e) {
+//                retry++;
+//            }
+//        }
+//        return error;}
+
+//    public void initializeGeneralErrorElement ( ) {
+//        errGeneral = SeleniumUtils.waitForGenericElement(driver , By.xpath("/html/body/div[5]/div/div/div/div/form/div/div[2]/div") , 15);
+//    }
 
     //    public void waitForLoginPage() {
 //        wait.until(ExpectedConditions.elementToBeClickable(Trimite));
@@ -77,12 +99,16 @@ public class LoginPage {
 
     public boolean checkErr (String error , String type) {
         if (type.equalsIgnoreCase("userErr"))
-            return error.equals(errUserName.getText());
+            return error.equals(SeleniumUtils.getElementMessage(driver, By.id("advice-required-entry-email")));
         else if (type.equalsIgnoreCase("passErr"))
-            return error.equals(errPassword.getText());
+            return error.equals(SeleniumUtils.getElementMessage(driver, By.id("advice-required-entry-pass")));
         else if (type.equalsIgnoreCase("generalErr"))
-            return error.equals(errGeneral.getText());
+            return error.equals(SeleniumUtils.getElementMessage(driver, By.xpath("//*[@id=\"top\"]/body/div[3]/div[3]/div[1]/div[3]/div/div[2]/div[2]/ul/li/ul/li/span")));
         return false;
+
+//        String err = SeleniumUtils.getElementMessage(driver, By.xpath("//*[@id='login-form']/form/div/div[2]/div"));
+//        Assert.assertEquals(err, genErrMsg);
+
     }
 
 
@@ -101,7 +127,7 @@ public class LoginPage {
     public void openLoginPage (String hostname) {
         System.out.println("Open the next url:" + hostname + "/customer/account/login/");
         driver.get(hostname + "/customer/account/login/");
-//                cookieMess.click();
+        cookieMess.click();
 
 
     }
